a73d38867c2b234fd0ceaa88f71009ddde939541
jade decordova <lacroixjaded@gmail.com>
2020-07-30 07:19:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find{ |call| call == valid_calls.include?(call)
-  return call
   }
 end
 
